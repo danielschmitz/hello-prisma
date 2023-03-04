1408534519979ab6115f4076aa288a139809fb7f
Daniel Schmitz <danieljfa@gmail.com>
2023-03-04 07:52:08 -0300

    fix

--- prisma/migrations/20230304105157_init/migration.sql
+++ prisma/migrations/20230304105157_init/migration.sql
@@ -9,7 +9,7 @@ CREATE TABLE "User" (
 CREATE TABLE "Post" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "title" TEXT NOT NULL,
-    "content" TEXT NOT NULL,
+    "content" TEXT,
     "published" BOOLEAN NOT NULL DEFAULT false,
     "authorId" TEXT NOT NULL,
     CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
